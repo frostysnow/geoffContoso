4d666679ce96249e08da5baa48c91d7ba8c7f238
geoffsim <geoffsim95@gmail.com>
2016-05-02 10:17:20 -0400

    Changed Student controller stuff with delete! Cool!

--- ContosoUniversity/ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentController.cs
@@ -114,11 +114,15 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Student/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete(int? id, bool? saveChangesError=false)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete Failed. Get better.";
             }
             Student student = db.Students.Find(id);
             if (student == null)
@@ -129,23 +133,22 @@ namespace ContosoUniversity.Controllers
         }
 
         // POST: Student/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete(int id)
         {
-            Student student = db.Students.Find(id);
-            db.Students.Remove(student);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
+            try
             {
-                db.Dispose();
+                Student student = db.Students.Find(id);
+                db.Students.Remove(student);
+                db.SaveChanges();
             }
-            base.Dispose(disposing);
+            catch (DataException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("Index");
         }
     }
 }
